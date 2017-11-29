928cfb4968d4854401408f656cadb9e838054234
Jacob Vigeveno <vigevenoj@gmail.com>
2017-11-29 15:39:54 -0800

    Add mqtt dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-quakes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Earthquakes and mqtt"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,6 +7,7 @@
                  [http-kit "2.2.0"]
                  [clj-http "3.7.0"]
                  [cheshire "5.8.0"]
+                 [clojurewerkz/machine_head "1.0.0"]
                  [com.climate/geojson-schema "0.2.1"]]
   :main ^:skip-aot clj-quakes.core
   :target-path "target/%s"
