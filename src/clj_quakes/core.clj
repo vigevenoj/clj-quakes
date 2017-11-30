ccd84fa810f599e52e219fb53bb740359c0cd4a0
Jacob Vigeveno <vigevenoj@gmail.com>
2017-11-30 08:36:52 -0800

    Refactor mqtt connection into main method
    Some code formatting changes, as well

--- src/clj_quakes/core.clj
+++ src/clj_quakes/core.clj
@@ -7,27 +7,41 @@
             [clojurewerkz.machine-head.client :as mh]
             [schema.core :as s]))
 
+(def ^:const owntracks-topic "owntracks/#")
+(def ^:const broker-url "tcp://sharkbaitextraordinaire.com:8885") ;; figure out how to use a TLS-secured connection
+
+(defn parse-owntracks
+  "Parse an mqtt payload into a map with keys"
+  [^bytes payload]
+  (let [update
+        (cheshire.core/parse-string (String. payload "UTF-8") true)]
+    (println update)
+    (println (:tst update))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let [id   (mh/generate-id)
+        conn (mh/connect broker-url id)]
+    (mh/subscribe conn {owntracks-topic 0}
+                  (fn [^String topic meta ^bytes payload]
+                    (parse-owntracks payload)))))
 
 (defn test-feed []
   (s/validate com.climate.geojson-schema.core/FeatureCollection
-  (cheshire.core/parse-string (slurp "/Users/vigevenoj/code/clojure/clj-quakes/hourly.json") true))
-  )
+              (cheshire.core/parse-string
+                (slurp "/Users/vigevenoj/code/clojure/clj-quakes/hourly.json") true)))
 
 ;; use this as a test point for haversine and such
 (def test-point
-  {:type "Point"
+  {:type        "Point"
    :coordinates [-122.58441925048828,
                  45.48420633926945]})
 
 (defn get-coordinates [feature]
   "Get the coordinates of a feature"
-  (:coordinates (:geometry feature))
-  )
+  (:coordinates (:geometry feature)))
 
 (defn get-longitude
   "Get the longitude from a GeoJSON Point"
@@ -42,31 +56,30 @@
 (defn hav
   "Calculate a haversine, sin^2(θ/2)"
   [val]
-  (Math/pow(Math/sin(/ val 2)) 2))
+  (Math/pow (Math/sin (/ val 2)) 2))
 
 (defn point-haversine
   "Haversine formula to calculate great-circle distance between two GeoJSON Point objects"
   [p1 p2]
-  (let [R 6371 ; radius of arth in km
-        delta-latitude (Math/toRadians (- (get-latitude p1) (get-latitude p2)))
+  (let [R               6371 ; radius of arth in km
+        delta-latitude  (Math/toRadians (- (get-latitude p1) (get-latitude p2)))
         delta-longitude (Math/toRadians (- (get-longitude p1) (get-longitude p2)))
-        latitude1 (Math/toRadians (get-latitude p1))
-        latitude2 (Math/toRadians (get-latitude p2))
-        a (+ (hav delta-latitude) (* (Math/cos latitude1) (Math/cos latitude2) (hav delta-longitude)))]
-    (* R 2 (Math/atan2 (Math/sqrt a) (Math/sqrt (- 1 a)))))
-  )
+        latitude1       (Math/toRadians (get-latitude p1))
+        latitude2       (Math/toRadians (get-latitude p2))
+        a               (+ (hav delta-latitude)
+                           (* (Math/cos latitude1) (Math/cos latitude2) (hav delta-longitude)))]
+    (* R 2 (Math/atan2 (Math/sqrt a) (Math/sqrt (- 1 a))))))
 
 (defn fetch-quakes []
   "Get the earthquake feed and parse it as a FeatureCollection"
   (let [response
         (cheshire.core/parse-string
-         (:body (client/get "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"))
-         true )]
-    (s/validate FeatureCollection response)
-    )
-  )
+         (:body
+           (client/get "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"))
+         true)]
+    (s/validate FeatureCollection response)))
 
-(defn distance-from-test 
+(defn distance-from-test
   "Calculate how far a point is from the test point"
   [point]
   (point-haversine test-point point))
@@ -76,24 +89,15 @@
 ;; and next step is to determine if the elements are within a range to be considered interesting
 ;; (map distance-from-test (map :geometry (:features (fetch-quakes))))
 
-(def ^:const owntracks-topic "owntracks/#")
-
-(defn connect-to-broker []
+(defn connect-to-broker [connectstring]
   (println "connecting to broker")
-  (mh/connect "tcp://sharkbaitextraordinaire.com:8885"))
-
-(defn parse-owntracks
-  "Parse an mqtt payload into a map with keys"
-  [^bytes payload]
-  (let [update
-  (cheshire.core/parse-string (String. payload "UTF-8") true)]
-    (println update)
-    (println (:tst update))))
+  (mh/connect connectstring))
 
 (def listen-for-owntracks
-  (let [c (connect-to-broker)]
-    (mh/subscribe c {owntracks-topic 0} (fn [^String topic meta ^bytes payload]
-                                          (parse-owntracks payload)))
+  (let [c (connect-to-broker "tcp://sharkbaitextraordinaire.com:8885")]
+    (mh/subscribe c {owntracks-topic 0}
+                  (fn [^String topic meta ^bytes payload]
+                    (parse-owntracks payload)))
     ;; use the location data above for calculating haversine distance between location update and earthquakes
     (println (mh/connected? c))
     (Thread/sleep 10000)
