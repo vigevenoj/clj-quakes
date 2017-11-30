2e2c421383bd7ee7f346299a5ad0ac2ffdcce6fa
Jacob Vigeveno <vigevenoj@gmail.com>
2017-11-30 15:48:50 -0800

    organize imports, add quartzite requirement

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]
+  :dependencies [[cheshire "5.8.0"]
                  [clj-http "3.7.0"]
-                 [cheshire "5.8.0"]
                  [clojurewerkz/machine_head "1.0.0"]
-                 [com.climate/geojson-schema "0.2.1"]]
+                 [clojurewerkz/quartzite "2.0.0"]
+                 [com.climate/geojson-schema "0.2.1"]
+                 [http-kit "2.2.0"]
+                 [org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clj-quakes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
