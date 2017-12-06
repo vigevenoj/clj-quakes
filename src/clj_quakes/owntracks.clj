1df78e6a859e0fad9936997306aa577d7f71fcef
Jacob Vigeveno <vigevenoj@gmail.com>
2017-12-05 20:39:48 -0800

    add function to parse owntracks updates

---
+++ src/clj_quakes/owntracks.clj
@@ -0,0 +1,8 @@
+(ns clj-quakes.owntracks
+ (:require [cheshire.core :refer :all]))
+
+(parse-update [json]
+  (let [point (cheshire.core/parse-string (String. json "UTF-8") true)]
+    {:type "Point"
+     :coordinates [(-> point :lon), (-> point :lat)]
+  }))
